04e24bacc95d87f57a79e24871ac9636868dd248
okunoyuto0000 <okunoyuto0000@gmail.com>
2021-03-23 07:20:33 +0000

    [Add] aboutページの完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
-  root to: 'homes#top'
-  get 'homes/about' => 'homes#about'
+  root to: 'home#top'
+  get 'home/about' => 'home#about'
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]
   resources :users, only: [:index, :show, :edit, :update]
 end
